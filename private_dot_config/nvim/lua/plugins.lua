b3e2780e6f4a06c8daae6877ead7f1b9ea6bf5b3
regencode <thomgozalie@gmail.com>
2025-01-26 18:09:06 +0000

    removed image.nvim

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -232,21 +232,4 @@ require("lazy").setup({
             })
         end
     },
-    {
-        "3rd/image.nvim",
-        config = function()
-            require('image').setup({
-                {
-                    backend = "kitty", -- Kitty will provide the best experience, but you need a compatible terminal
-                    integrations = {}, -- do whatever you want with image.nvim's integrations
-                    max_width = 100,
-                    max_height = 100,
-                    max_height_window_percentage = math.huge, -- this is necessary for a good experience
-                    max_width_window_percentage = math.huge,
-                    window_overlap_clear_enabled = true,
-                    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-                },
-            })
-        end,
-    },
 })
